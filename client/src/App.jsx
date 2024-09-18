6b8aadbdd0db33fa3d4df16b642a6bfcbcf2f56f
Nikhil Kumar <nikhilkumar232001@gmail.com>
2024-09-18 17:04:16 +0530

    Added Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Signin from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Dashboard from './pages/Dashboard'
+import Header from './Components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+     <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
