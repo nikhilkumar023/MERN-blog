53d069a068ee3ff91ea94d55c8bf57dc17700134
Nikhil Kumar <nikhilkumar232001@gmail.com>
2024-09-19 22:46:13 +0530

    use functionality to the sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,29 +1,29 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
-    const { username, email, password } = req.body;
+  console.log("entered in signup");
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === '' || email === '' || password === '') {
-        next(errorHandler(400, 'All fields are required'));
-    }
+  if (!username || !email || !password || username === "" || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword,
-    });
+  const newUser = new User({
+    username,
+    email,
+    password,
+  });
 
-    try {
-
-        await newUser.save();
-        res.json('Signup successful');
-
-    } catch (error) {
-        next(error);
-    }
-
-    };
+  try {
+    console.log("in try");
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) 
+  {
+    next(error);
+  }
+};
