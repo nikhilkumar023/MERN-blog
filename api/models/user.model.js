0e7db1bdfc2cffb19bc1692bff3c2e3db851d9ff
Nikhil Kumar <nikhilkumar232001@gmail.com>
2024-09-26 23:40:23 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -6,19 +6,23 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true,
     },
-    password:{
+    password: {
         type: String,
         required: true,
     },
-     profilePicture:{
+    profilePicture: {
         type: String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
     },
+    isAdmin: {
+        type:Boolean,
+        default: false,
+    }, 
     }, {timestamps: true}
 );
 
