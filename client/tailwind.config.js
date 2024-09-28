b90bdc1cd96ca35bbce1015e89871b98d27d300d
Nikhil Kumar <nikhilkumar232001@gmail.com>
2024-09-28 13:56:47 +0530

    add show more functionality to the posts results of the dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
 const flowbite = require("flowbite-react/tailwind");
+import scrollBar from "tailwind-scrollbar";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [ flowbite.plugin(), require('tailwind-scrollbar')],
-}
+  plugins: [flowbite.plugin(), scrollBar],
+};
