1b868724422df7627eec003d181c451e9fcbc7d3
Nikhil Kumar <nikhilkumar232001@gmail.com>
2024-09-30 03:25:15 +0530

    add call to action to the post page

---
+++ client/src/Components/CallToAction.jsx
@@ -0,0 +1,29 @@
+//import React from 'react'
+
+import { Button } from "flowbite-react"
+
+function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+        <div className="flex-1 justify-center flex flex-col">
+        
+            <h2 className="text-2xl"> 
+                want to Search?
+            </h2>
+            <p className="text-gray-500 my-2">
+                Checkout these resources with Google 
+            </p>
+            <Button gradientDuoTone='purpleToPink' className="rounded-tl-xl rounded-bl-none">
+                <a href="https://www.google.com" target='_blank' rel='noopener noreferrer'>
+                    Google
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        </div>
+    </div>
+  )
+}
+
+export default CallToAction
